60a8e98158f076d5fa2f219767d0e3affdbf7ba9
bourrett <thais.bourret@grenoble-inp.org>
2023-11-20 18:12:50 +0100

    gaussian_map.py

--- gaussian_map.py
+++ gaussian_map.py
@@ -2,7 +2,6 @@ import openmesh as om
 import math 
 import numpy as np
   
-# returns square of distance b/w two openmesh vertices
 def dist(mesh,x1, x2):  
     x1 = mesh.point(x1)
     x2 = mesh.point(x2)
@@ -11,61 +10,50 @@ def dist(mesh,x1, x2):
     zDiff = x1[2] - x2[2]  
     return np.sqrt(xDiff * xDiff + yDiff * yDiff + zDiff * zDiff)
 
-def comput_angles(mesh, A, B, C):  
-      
+def compute_angles(mesh, A, B, C):  
+    ''' Compute the angles of the given triangle'''
     # length of sides be a, b, c  
     a = dist(mesh, B,C)  
     b = dist(mesh, A,C)    
     c = dist(mesh, A,B)
-  
-    # From Cosine law  
-    alpha = math.acos((b**2 + c**2 - a**2) /
-                         (2 * b * c));  
-    betta = math.acos((a**2 + c**2 - b**2) / 
-                         (2 * a * c));  
-    gamma = math.acos((a**2 + b**2 - c**2) / 
-                         (2 * a * b));  
-  
-    # Converting to degree  
-    # alpha = alpha * 180 / math.pi;  
-    # betta = betta * 180 / math.pi;  
-    # gamma = gamma * 180 / math.pi;  
-  
-    return alpha,betta,gamma
 
-def comput_area(mesh, face_vertices_dict, face_vertices_angles, face_id):
-        A,B,C = face_vertices_dict[face_id]
-        angle = face_vertices_angles[face_id][A.idx()]
-        b = dist(mesh, A,C)
-        c = dist(mesh, A,B)
-        area = b * c * np.sin(angle) / 2
-        return area
+    # Cosinus law  
+    alpha = math.acos((b**2 + c**2 - a**2) / (2 * b * c))
+    beta = math.acos((a**2 + c**2 - b**2) / (2 * a * c))
+    gamma = math.acos((a**2 + b**2 - c**2) / (2 * a * b))
 
+    return alpha,beta,gamma
 
+def compute_area(mesh, face_vertices_dict, face_vertices_angles, face_id):
+    ''' Compute the area of the face given'''
+    A,B,C = face_vertices_dict[face_id]
+    angle = face_vertices_angles[face_id][A.idx()]
+    b = dist(mesh, A,C)
+    c = dist(mesh, A,B)
+    area = b * c * np.sin(angle) / 2
+    return area
           
 
 def add_angles(mesh):
     '''This function aim to store for every face of our mesh, the vertices associated and the angle associated to
-    
     each verticies in the face'''
     face_vertices_dict = {} 
     face_vertices_angles = {}
     for face in mesh.faces(): #Iterate on the face of our mesh
         # print(f"Face {face.idx()}: ", end="")
-        vertices_ids = [v for v in mesh.fv(face)]  # Get vertex  of the face
-        face_vertices_dict[face.idx()] = vertices_ids  # Store face ID as key and a list of the 3 [vertex ID, vertex's coord] as value
+        vertices = [v for v in mesh.fv(face)]  # Get vertex of the face
+        face_vertices_dict[face.idx()] = vertices  # Store face ID as key and the verteices from the face as value
 
     for face in mesh.faces():
-        vertices = face_vertices_dict[face.idx()] #get the 3 vertices id and coord of our face
-        a,b,c = comput_angles(mesh, vertices[0], vertices[1],vertices[2]) #comput the angles of our triangle
-        angles = [a, b, c] #store the 3 angles associated to our 3 vertices
+        vertices = face_vertices_dict[face.idx()] #get the 3 vertices our face
+        a,b,c = compute_angles(mesh, vertices[0], vertices[1],vertices[2]) #comput the angles of our triangle
         vertices_angles = {vertices[0].idx(): a, vertices[1].idx() : b, vertices[2].idx(): c}
         face_vertices_angles[face.idx()] = vertices_angles #Store face ID as key and a list of the 3 [vertex ID, vertex's angle] as value    
     return face_vertices_dict, face_vertices_angles
 
 def get_opposite_vertices(face_vertices_dict, face_id1, face_id2, vertex):
-    '''this function return the vertex id of the vertex also being in face1 and face2 but not being vertex_id 
-    and also return the  two vertices ids of the vremaining vertices in the faces'''
+    '''this function return the vertex id of the vertex being in face1 and face2 but not being vertex_id 
+    and also return the  two vertices ids of the two remaining vertices in the faces'''
     vertices_face1 = []
     vertices_face2 = []
     opp_vertices = []
@@ -91,75 +79,57 @@ def get_opposite_vertices(face_vertices_dict, face_id1, face_id2, vertex):
 def cotan(x):
     return np.cos(x)/np.sin(x)
 
+def map_curvature_to_color(curvature_value):
+    if curvature_value > 0:
+        return (1.0, 0.0, 0.0, 1.0)  # Red color for positive curvature
+    elif curvature_value < 0:
+        return (0.0, 0.0, 1.0, 1.0)  # Blue color for negative curvature
+    else:
+        return (0.0, 1.0, 0.0, 1.0)  # Green color for zero curvature (or any default color)
+
 
 def mean_curvature(mesh, face_vertices_dict, face_vertices_angles):
-    #mesh is a openmesh mesh
-    gaussian_curvature_values = []
+    #vertex_curvature = mesh.vertex_property("float")  
     for vertex in mesh.vertices(): #iterate on the vertices of our mesh
+        #print("New Vertex -----------------------------------------")
         sum = 0
         Ai = 0
         faces_ids = [face.idx() for face in mesh.vf(vertex)] #Store the ids of the faces adjacent to our vertex
+        if len(faces_ids) == 1:
+            #Vertex is only in one face -> Boundary vertex what shoud we do?
+            #TODO
+            continue
         for i in range(len(faces_ids)-1):
-            #print("face n°", i)
+            #print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>face n°", i)
             face1 = faces_ids[i]
             face2 = faces_ids[i+1]
             opp_vertices = get_opposite_vertices(face_vertices_dict, face1, face2, vertex)
-            # print(opp_vertices)
-           # print(face_vertices_angles[face1])
+            #print(opp_vertices)
+            #print(face_vertices_angles[face1])
             #print(face_vertices_angles[face1][opp_vertices[0]])
             cot_weight = cotan(face_vertices_angles[face1][opp_vertices[1]]) + cotan(face_vertices_angles[face2][opp_vertices[2]])
-            pj = mesh.point(opp_vertices[0]) 
+            pj = mesh.point(opp_vertices[0]) #get the coord. of the opposite vertex
             sum += cot_weight*(pj - mesh.point(vertex))
-            face1_area = comput_area(mesh, face_vertices_dict, face_vertices_angles, face1)
-            face2_area = comput_area(mesh, face_vertices_dict, face_vertices_angles, face2)
-            print(face1_area)
-
-
-############################################################################
-
-# Assuming you have a triangle mesh defined by vertices and faces
-# Replace these placeholders with your actual mesh data
-
-vertices = [...]  # Your mesh vertices
-faces = [...]     # Faces (triangles or polygons) defining the mesh
-
-def compute_discrete_gaussian_curvature(mesh_vertices, mesh_faces):
-    gaussian_curvature_values = []
-
-    for vertex_idx, vertex in enumerate(mesh_vertices):
-        adjacent_faces = np.where(np.any(mesh_faces == vertex_idx, axis=1))[0]
-
-        vertex_gaussian_curvature = 0.0
-        for face_idx in adjacent_faces:
-            face = mesh_faces[face_idx]
-            vertex_face_indices = np.where(face == vertex_idx)[0]
-
-            if len(vertex_face_indices) == 0:
-                continue
-
-            opposite_vertex_indices = np.delete(face, vertex_face_indices)
-            a = mesh_vertices[opposite_vertex_indices[0]] - vertex
-            b = mesh_vertices[opposite_vertex_indices[1]] - vertex
-
-            angle = np.arccos(np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b)))
-            vertex_gaussian_curvature += angle
-
-        # Calculate discrete Gaussian curvature
-        vertex_gaussian_curvature = (2 * np.pi - vertex_gaussian_curvature) / np.linalg.norm(np.sum(mesh_vertices[mesh_faces[adjacent_faces]], axis=0) / len(adjacent_faces))
-
-        gaussian_curvature_values.append(vertex_gaussian_curvature)
-
-    return gaussian_curvature_values
-
-# Calculate discrete Gaussian curvature values for your mesh
-# discrete_gaussian_curvature_values = compute_discrete_gaussian_curvature(vertices, faces)
-# print("Discrete Gaussian curvature values:", discrete_gaussian_curvature_values)
+            face1_area = compute_area(mesh, face_vertices_dict, face_vertices_angles, face1)
+            face2_area = compute_area(mesh, face_vertices_dict, face_vertices_angles, face2)
+            Ai += face1_area + face2_area
+        # print(face1_area)
+        # print(face2_area)
+        Ai *= 1/3
+        # print("Ai = ", Ai)
+        vertex_curvature = np.linalg.norm(sum/(2*Ai))/2
+        #print("vertex_curvature =", vertex_curvature)
+        # mesh.set_vertex_property('prop', vertex, vertex_curvature )
+        color = map_curvature_to_color(vertex_curvature)
+        mesh.set_color(vertex, color)
+        mesh.get_color(vertex)
+    return mesh
 
 
 ######################################################################
-def main2():   #Vraie main qui utilisera des fichiers .off
+def main2():   #Real main reading .off files
     print("---------- Début main\n")
-    off_file = "Objects/test.off"
+    off_file = "Objects/sphere.off"
     #à la main:
     #read_off_file(off_file)
     # En utilisnt les librairies:
@@ -167,16 +137,16 @@ def main2():   #Vraie main qui utilisera des fichiers .off
     #om.write_mesh('test.off', mesh) #Ecrit un fichier off à partir d'un mesh
     # TODO implémenter algorithme
     a, b = add_angles(mesh)
+    mesh.request_face_colors()
     mean_curvature(mesh, a, b)
-    #mean_curvature(mesh)
+    om.write_mesh("test_sphere.off", mesh)
     print("---------- Fin main\n")
 
 
-def main():  # Dummy main avec mesh basique pour tester nos fonctions
+def main():  # Dummy main with basic mesh to test the functions
     print("---------- Début main\n")
-    mesh = om.TriMesh()
-
-    # add a a couple of vertices to the mesh
+    mesh = om.TriMesh() #créer un mesh
+    #initialize a mesh with 4 vertices
     vh0 = mesh.add_vertex([0, 1, 0])
     vh1 = mesh.add_vertex([1, 0, 0])
     vh2 = mesh.add_vertex([2, 1, 0])
@@ -187,14 +157,17 @@ def main():  # Dummy main avec mesh basique pour tester nos fonctions
     fh0 = mesh.add_face(vh0, vh1, vh2)
     #fh1 = mesh.add_face(vh1, vh3, vh4)
     fh2 = mesh.add_face(vh0, vh3, vh1)
-    # add another face to the mesh, this time using a list
     #vh_list = [vh2, vh1, vh4]
     #fh3 = mesh.add_face(vh_list)
     # TODO implémenter algorithme
     a, b = add_angles(mesh)
-    mean_curvature(mesh, a, b)
+    mesh.request_face_colors() # pour ajouter des couleurs aux faces?
+    new_mesh = mean_curvature(mesh, a, b)
+    # mesh.get_color
+
+    om.write_mesh("test_curvature.off", mesh) # write .off file
     #mean_curvature(mesh)
     print("---------- Fin main\n")
 
 if __name__ == "__main__":
-    main()  # Call the main function when this file is executed directly
+    main2()  # Call the main function when this file is executed directly
